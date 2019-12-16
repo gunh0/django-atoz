40408d1e05e7a0973a87f8a12061cb3bb8b81e6f
devgunho <parkgh3737@naver.com>
2019-12-16 09:33:50 +0900

    commit

---
+++ BookMark/mysite/bookmark/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+#from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
+
+# Create your models here.
+
+@python_2_unicode_compatible    # Python 2.x 지원용
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    url = models.URLField('url', unique=True)
+    
+    def __str__(self):  # 객체를 문자열로 표현할 때 사용하는 함수
+        return self.title
