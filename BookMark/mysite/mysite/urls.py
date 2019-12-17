26f299d1846e080bed78bbeb0f907602e081f2ca
Gunho Park <devgun@kakao.com>
2019-12-17 22:01:02 +0900

    commit

--- BookMark/mysite/mysite/urls.py
+++ BookMark/mysite/mysite/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from bookmark.views import BookmarkLV, BookmarkDV
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    # Class-based views for Bookmark app
+    url(r'^bookmark/$',BookmarkLV.as_view(),name='index'),
+    url(r'^bookmark/(?P<pk>\d+)/$',BookmarkDV.as_view(),name='detail'),
 ]
