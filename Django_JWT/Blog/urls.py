f9c24298e395d8174e3e8df1497ed6daeeb58a16
devgunho <parkgh3737@naver.com>
2020-02-06 14:15:07 +0900

    Add Create Blog Post / Error Detail

--- Django_JWT/Blog/urls.py
+++ Django_JWT/Blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.posts, name='posts'),
     path('post/<int:id>/', views.post_detail, name='post_detail'),
+    path('post/create/', views.post_create, name='post_create'),
 ]
