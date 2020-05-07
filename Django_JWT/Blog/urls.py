802c4ae07548eec82d428ed28124b760f158ce75
Gunho Park <devgun@kakao.com>
2020-05-07 18:17:13 +0900

    commit

--- Django_JWT/Blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.posts, name='posts'),
-    path('post/<int:id>/', views.post_detail, name='post_detail'),
-    path('post/create/', views.post_create, name='post_create'),
-]
